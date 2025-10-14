a9a632f650ab226545633b5d9993a6e1739f9884
Sega1A <sergio.garciacuchallo@gmail.com>
2025-10-14 18:34:15 -0400

    feature: create an admin user if it was not created before

--- banana-code/src/app/page.tsx
+++ banana-code/src/app/page.tsx
@@ -1,10 +1,12 @@
+import { ensureAdminExists } from "@/scripts/ensureAdmin";
 import LoginForm from "../app/components/login-form";
 
 export default function LoginPage() {
+  // Create an Admin user
+  ensureAdminExists();
   return (
     <main className="min-h-screen bg-gradient-to-br from-yellow-50 to-amber-100 flex items-center justify-center p-4">
       <LoginForm />
     </main>
   );
 }
-
