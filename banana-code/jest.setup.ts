148d2419ca7239b7edcc68920c2eb72a375b08f8
Dayver Condori <202103818@est.umss.edu>
2025-10-21 14:15:02 -0400

    feature: update Jest configuration and add polyfills for Request and Response

--- banana-code/jest.setup.ts
+++ banana-code/jest.setup.ts
@@ -1,1 +1,19 @@
-// import "@testing-library/jest-dom";
+// Minimal Request polyfill for next/server which expects Request in test env
+if (typeof (globalThis as any).Request === "undefined") {
+	(globalThis as any).Request = class Request {
+		constructor(input: any, init?: any) {
+			(this as any).input = input;
+			(this as any).init = init;
+		}
+	};
+}
+
+// Minimal Response polyfill (used by next/server internals in tests)
+if (typeof (globalThis as any).Response === "undefined") {
+	(globalThis as any).Response = class Response {
+		constructor(body?: any, init?: any) {
+			(this as any).body = body;
+			(this as any).init = init;
+		}
+	};
+}
