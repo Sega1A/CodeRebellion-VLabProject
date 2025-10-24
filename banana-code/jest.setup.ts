df4b6b09b35a26db2a8976a1fea836ff1763ed49
Katruyn Morató Laura <katyosethmorato@gmail.com>
2025-10-24 19:18:50 -0400

    refactor: enhance polyfills for Request and Response in jest.setup.ts

--- banana-code/jest.setup.ts
+++ banana-code/jest.setup.ts
@@ -1,19 +1,47 @@
 // Minimal Request polyfill for next/server which expects Request in test env
-if (typeof (globalThis as any).Request === "undefined") {
-	(globalThis as any).Request = class Request {
-		constructor(input: any, init?: any) {
-			(this as any).input = input;
-			(this as any).init = init;
+interface GlobalThisWithPolyfills {
+	Request?: unknown;
+	Response?: unknown;
+}
+
+type PolyfillRequestInput = string | URL;
+type PolyfillRequestInit = {
+	method?: string;
+	headers?: Record<string, string>;
+	body?: unknown;
+	[key: string]: unknown;
+};
+
+type PolyfillResponseInit = {
+	status?: number;
+	statusText?: string;
+	headers?: Record<string, string>;
+	[key: string]: unknown;
+};
+
+if (typeof (globalThis as GlobalThisWithPolyfills).Request === "undefined") {
+	(globalThis as GlobalThisWithPolyfills).Request = class PolyfillRequest {
+		input: PolyfillRequestInput;
+		init?: PolyfillRequestInit;
+		
+		constructor(input: PolyfillRequestInput, init?: PolyfillRequestInit) {
+			this.input = input;
+			this.init = init;
 		}
 	};
 }
 
 // Minimal Response polyfill (used by next/server internals in tests)
-if (typeof (globalThis as any).Response === "undefined") {
-	(globalThis as any).Response = class Response {
-		constructor(body?: any, init?: any) {
-			(this as any).body = body;
-			(this as any).init = init;
+if (typeof (globalThis as GlobalThisWithPolyfills).Response === "undefined") {
+	(globalThis as GlobalThisWithPolyfills).Response = class PolyfillResponse {
+		body?: unknown;
+		init?: PolyfillResponseInit;
+		
+		constructor(body?: unknown, init?: PolyfillResponseInit) {
+			this.body = body;
+			this.init = init;
 		}
 	};
 }
+
+
